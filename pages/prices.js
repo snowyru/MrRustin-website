9cdb1b46824ece28d4fef2ae2a01272eb8566ed4
snowyru <97807830+snowyru@users.noreply.github.com>
2022-09-15 14:22:54 +0200

    reverted index.js and created prices.js

---
+++ pages/prices.js
@@ -0,0 +1,14 @@
+import {motion} from 'framer-motion';
+import {Typography} from '@mui/material';
+
+
+const prices = () => {
+    return ( 
+        <>
+        
+
+        </>
+     );
+}
+ 
+export default prices;
